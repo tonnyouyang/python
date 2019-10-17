6dc03abc4cfe784bef7eaee98041afbbbbd4c424
tonnyouyang <tonny_ouyang@163.com>
2019-10-17 23:59:17 +0800

    1017

--- io_use.py
+++ io_use.py
@@ -13,14 +13,15 @@ f.close()
 # 如果没有特别指定，
 # 将假定启用默认的阅读（'r'ead）模式
 f = open('poem.txt')
-while True:
-    line = f.readline()
-    # 零长度指示 EOF
-    if len(line) == 0:
-        break
-# 每行（`line`）的末尾
-# 都已经有了换行符
-# 因为它是从一个文件中进行读取的
-print(line, end='')
-# 关闭文件
+for line in f:
+    print(line, end='')
+# while f.readline()：
+#     print(f.readline(), end='')
+# #     # 零长度指示 EOF
+# #     if len(line) == 0:
+# #         break
+# # # 每行（`line`）的末尾
+# # # 都已经有了换行符
+# # # 因为它是从一个文件中进行读取的
+# # 关闭文件
 f.close()
