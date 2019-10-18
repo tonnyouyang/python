166c7d15936919e8d3af8e9d6a20e838d39fcbf9
tonnyouyang <56437221+tonnyouyang@users.noreply.github.com>
2019-10-19 00:22:33 +0800

    8

--- io_use.py
+++ io_use.py
@@ -1,15 +1,15 @@
-poem = '''\
-Programming is fun
-When the work is done
-if you wanna make your work also fun:
-use Python!
-'''
-# 打开文件以编辑（'writing）
-f = open('poem.txt', 'w')
-# 向文件中编写文本
-f.write(poem)
-# 关闭文件
-f.close()
+# poem = '''\
+# Programming is fun
+# When the work is done
+# if you wanna make your work also fun:
+# use Python!
+# '''
+# # 打开文件以编辑（'writing）
+# f = open('poem.txt', 'w')
+# # 向文件中编写文本
+# f.write(poem)
+# # 关闭文件
+# f.close()
 # 如果没有特别指定，
 # 将假定启用默认的阅读（'r'ead）模式
 for line in open('poem.txt'):
